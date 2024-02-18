37e94cf4fad5cc8480d97ee182598281ad0555e7
Sanjay0070 <153261706+Sanjay0070@users.noreply.github.com>
2024-02-18 20:24:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123345
+let accountEmail = "sanjay123@gmail.com"
+var accountPassword = "123321"
+accountCity = "Gorakhpur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "ghjk.com"
+accountPassword = "345543"
+accountCity = "Delhi"
+
+console.log(accountEmail, accountId, accountPassword, accountCity)
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use stric"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3) // code readibility is high 
+
+
+console.log("aa");
+
+
+let name = "Sanjay"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
